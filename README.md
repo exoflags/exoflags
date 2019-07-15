fb049564c2fece9991753717e5d37a5d217caeed
Tom Valorsa <tom.valorsa@arup.com>
2019-07-15 13:48:36 +0100

    Create project from single-screen template

---
+++ README.md
@@ -0,0 +1,3 @@
+# Arup App
+
+This app was bootstrapped with `create-react-app` and `arup-scripts`.
---
+++ src/theme.js
@@ -0,0 +1,17 @@
+const theme = {
+  borderRadius: 4,
+  transitionDuration: {
+    short: '0.3s'
+  },
+  colors: {
+    white: '#FFFFFF',
+    black: '#212121',
+    grey: {
+      light: '#EEEEEE',
+      medium: '#7F8FA4',
+      dark: '#354052'
+    }
+  }
+};
+
+export default theme;
